a064271a3680a76830e35274d7941b7491071f0a
kemiashaju <kemiolugbogi0@gmail.com>
2025-11-13 15:05:08 +0100

    Header

--- alx-project-0x01/components/layout/Header.tsx
+++ alx-project-0x01/components/layout/Header.tsx
@@ -1,3 +1,25 @@
-export default function Header() {
-  return <header>Header Section</header>;
+import Link from 'next/link'; 
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-blue-600 text-white shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <h3 className="font-bold text-2xl">
+     <Link href="/">Daily Contents</Link>
+  </h3>
+        <nav>
+          <ul className="flex space-x-6">
+            <li className="hover:underline">
+              <Link href="/posts">Posts</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
+
+export default Header;
